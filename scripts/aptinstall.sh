c72e9e1c0b3ac2bd44590a5a92aad166f0504483
Tommaso Patrizi <tommasop@thinkingco.de>
2020-06-04 17:30:20 +0200

    Create aptinstall.sh

---
+++ scripts/aptinstall.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:ubuntu-ppa/backports
+sudo apt update && sudo apt full-upgrade -y
+
+function install {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo apt install -y $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+# Basics
+install awscli
+install chromium-browser
+install curl
+install dialog
+install exfat-utils
+install file
+install git
+install htop
+install jq
+install nmap
+install openvpn
+install tmux
+install tree
+install xclip
+install zsh
+
+# Image processing
+install jpegoptim
+install optipng
