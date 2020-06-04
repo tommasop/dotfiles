a77ad2549358193f0dde53702dbb969817f6fffa
Tommaso Patrizi <tommasop@thinkingco.de>
2020-06-04 17:31:45 +0200

    Create setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update Ubuntu and get standard repository programs
+./aptinstall.sh
+
+# Run all programs/ install scripts
+for f in programs/*.sh; do bash "$f" -H; done
+
+# Get all upgrades
+sudo apt upgrade -y
+
+# See our zsh changes
+source ~/.zshrc
